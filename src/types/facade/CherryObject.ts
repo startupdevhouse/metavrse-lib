c554a71463c74af4e9d9582a4fa00ef64fa9925f
dawidsdh <dawid.schatton@start-up.house>
2022-03-08 10:49:16 +0100

    refactor: MET-1716 change types and add setAssets method

--- src/types/facade/CherryObject.ts
+++ src/types/facade/CherryObject.ts
@@ -1,8 +1,8 @@
 import { CherryProjectManagerObject } from '../cherry/CherryProjectManagerObject';
 import { CherryObjectMeshes } from './CherryObjectMeshes';
 import { CherryObjectAnimations } from './CherryObjectAnimations';
-import { CherryObjectInfo } from './CherryObjectInfo';
 import { CherryObjectComponents } from './CherryObjectComponents';
+import { CherryObjectInfo } from '../cherry/CherryObjectInfo';
 
 export type CherryObject = {
   object: CherryProjectManagerObject;

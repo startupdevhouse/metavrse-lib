f9418d9722c3da0b6277020f75f519eedbf51bd1
dawidsdh <dawid.schatton@start-up.house>
2022-04-01 09:22:12 +0200

    feat: MET-2025 color picker types

--- src/types/cherry/CherryProjectManagerObject.ts
+++ src/types/cherry/CherryProjectManagerObject.ts
@@ -5,6 +5,36 @@ import { TreeNodeType } from '../tree/TreeNodeType';
 import { CherryKey } from './CherryKey';
 import { CherryMesh } from './CherryMesh';
 
+export type GetterSetterPropertyType =
+  | 'position'
+  | 'scale'
+  | 'rotate'
+  | 'groupMat'
+  | 'anchor'
+  | 'hud'
+  | 'pivot'
+  | 'visible'
+  | 'show_shadow'
+  | 'cast_shadow'
+  | 'front_facing'
+  | 'autoscale'
+  | 'controller'
+  | 'frame'
+  | 'mesh'
+  | 'finalTransformation'
+  | 'finalVisibility'
+  | 'parentOpts'
+  | 'animation'
+  | 'animations'
+  | 'hudscale';
+
+export type ProjectManagerObjectPropertyType =
+  | string
+  | boolean
+  | number
+  | Vector3
+  | RGB;
+
 /**
  * @description ProjectManager.getObject(key) result
  */
@@ -28,34 +58,45 @@ export type CherryProjectManagerObject = {
   removeChangeListener: (callback: any) => void;
   clearChangeHandlers: () => void;
 
-  buckets: any;
-  children: any;
-  meshdata: Map<unknown, unknown>;
+  /** @description Getter & Setter */
+  position: Vector3;
+  scale: Vector3;
+  rotate: Vector3;
+  groupMat: number[];
+  anchor: Vector3;
+  hud: boolean;
+  pivot: Vector3;
+  visible: boolean;
+  show_shadow: boolean;
+  cast_shadow: boolean;
+  front_facing: boolean;
+  autoscale: number;
+  controller: string;
+  frame: number;
   /** @description Use to retrive mesh specify by index or update mesh by it index */
   mesh: {
     get: (index: number, property: ShaderParameterType) => CherryMesh;
     set: (index: number, property: ShaderParameterType, value: unknown) => void;
   };
-
-  animation: CherryAnimation;
-  color: RGB;
-  groupMat: number[];
-  intensity: number;
-  position: Vector3;
-  visible: boolean;
-  finalPosition: Vector3;
+  finalTransformation: Float32Array;
   finalVisibility: boolean;
   parentOpts: { visible: boolean; transforms: Vector3; transform: any };
+  animation: CherryAnimation;
+  animations: any;
+  hudscale: Vector3;
+
+  /** @description Additional parameters */
+  buckets: any;
+  children: any;
+  meshdata: Map<unknown, unknown>;
+  color: RGB;
+  intensity: number;
+  finalPosition: Vector3;
   transparent: boolean;
-  pivot?: Vector3;
   item: {
     type: TreeNodeType;
     title: string;
     key: CherryKey;
   };
-  scale: Vector3;
-  rotate: Vector3;
-  autoscale: number;
-  finalTransformation: Float32Array;
-  parent?: any;
+  parent: CherryProjectManagerObject;
 };

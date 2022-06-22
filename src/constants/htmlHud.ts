c67858a1333a09e7df9abd0b9be6f5ccf301e213
Michał Kozłowski <michal.kozlowski@start-up.house>
2022-06-22 17:14:03 +0200

    fix: MET-2266 PR changes

--- src/constants/htmlHud.ts
+++ src/constants/htmlHud.ts
@@ -1,13 +1,14 @@
 import { HTMLHudElementType, HTMLHudSupportedTags } from '../types';
 
+const createDropTypes = <T extends string>(arr: Array<T> | ReadonlyArray<T>) =>
+  arr.map((t) => `htmlHud.${t}`) as Array<`htmlHud.${T}`>;
+
 export const HTML_HUD_SUPPORTED_TAG_NAMES: ReadonlyArray<HTMLHudSupportedTags> =
   ['div', 'img', 'input'];
 
-export const HTML_HUD_SUPPORTED_DROP_TYPES = <const>[
-  'htmlHud.div',
-  'htmlHud.img',
-  'htmlHud.input',
-];
+export const HTML_HUD_SUPPORTED_DROP_TYPES = createDropTypes(
+  HTML_HUD_SUPPORTED_TAG_NAMES
+);
 
 export const HTML_HUD_ELEMENT_TYPE_NAME_MAP: Record<
   HTMLHudElementType,
@@ -42,6 +43,6 @@ export const HTML_HUD_EMPTY_ELEMENTS = <const>[
   'wbr',
 ];
 
-export const HTML_HUD_EMPTY_ELEMENTS_DROP_TYPES = HTML_HUD_EMPTY_ELEMENTS.map(
-  (t) => `htmlHud.${t}`
-) as Array<`htmlHud.${typeof HTML_HUD_EMPTY_ELEMENTS[number]}`>;
+export const HTML_HUD_EMPTY_ELEMENTS_DROP_TYPES = createDropTypes(
+  HTML_HUD_EMPTY_ELEMENTS
+);
